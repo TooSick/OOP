03e9d6639e882e4a53f0118c76646224a9bde8ca
TooSick <meshchenya000@gmail.com>
2021-06-14 13:09:34 +0300

    OOP laba5 end

---
+++ Laba2/Factory/PolygonCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    class PolygonCreator : IShapesCreator
+    {
+        public Shapes CreateShape()
+        {
+            return new Polygon();
+        }
+    }
+}
